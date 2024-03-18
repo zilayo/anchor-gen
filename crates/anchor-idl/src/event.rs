7736673be9de66d22c561699adfc8c417c70b1ce
zilayo <84344709+zilayo@users.noreply.github.com>
2024-03-18 00:32:27 +0000

    fix: fix discriminator 5

--- crates/anchor-idl/src/event.rs
+++ crates/anchor-idl/src/event.rs
@@ -20,15 +20,9 @@ pub fn generate_events(
                 let opts = struct_opts.get(&def.name).copied().unwrap_or_default();
 
                 let discriminator: proc_macro2::TokenStream = {
-                    let discriminator_preimage = format!("event:{}", struct_name);
-                    let mut hash = Sha256::new();
-                    hash.update(discriminator_preimage.as_bytes());
-                    let discriminator_bytes = &hash.finalize()[..8];
-                    let discriminator_vec = discriminator_bytes
-                        .iter()
-                        .map(|byte| quote!(#byte))
-                        .collect::<Vec<_>>();
-                    quote!([#(#discriminator_vec),*])
+                    let discriminator_preimage = format!("event:{}", struct_name).into_bytes();
+                    let discriminator = anchor_syn::hash::hash(&discriminator_preimage);
+                    format!("{:?}", &discriminator.0[..8]).parse().unwrap()
                 };
 
                 let fields = def
@@ -47,10 +41,16 @@ pub fn generate_events(
 
                     impl anchor_lang::Discriminator for #struct_name {
                         const DISCRIMINATOR: [u8; 8] = #discriminator;
-                        fn discriminator() -> [u8; 8] {
-                            #discriminator
-                        }
                     }
+
+                    impl anchor_lang::Event for #struct_name {
+                      fn data(&self) -> Vec<u8> {
+                          let mut data = Vec::with_capacity(256);
+                          data.extend_from_slice(&#discriminator);
+                          self.serialize(&mut data).unwrap();
+                          data
+                      }
+                  }
                 }
             });
             quote! {
